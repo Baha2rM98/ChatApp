10b01af95294f96d67536d3586c63c793c4daf99
Baha2r <baha2r.mirzazadeh98@gmail.com>
2019-12-05 03:05:54 +0330

    Constructor created, essential methods added

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,9 +1,42 @@
 package Client;
 
+import java.io.*;
+import java.net.*;
+
 /**
  * @author Baha2r
  * Date: 12/05/2019 02:49 AM
  **/
 
 public class Client {
+    private Socket client;
+    private InputStream input;
+    private OutputStream output;
+    private String name = "";
+
+    public Client(Socket socket, DataInputStream input, DataOutputStream output) {
+        this.client = socket;
+        this.input = input;
+        this.output = output;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public InputStream getInput() {
+        return this.input;
+    }
+
+    public OutputStream getOutput() {
+        return this.output;
+    }
+
+    public Socket getClient() {
+        return this.client;
+    }
 }
