2ee0b3cecfe33a64e9d2a1843dd03de858b1cca5
Baha2r <baha2r.mirzazadeh98@gmail.com>
2019-12-05 20:02:26 +0330

    OnDisconnected interfaces package and name  changed

---
+++ src/ServerSide/OnDisconnected.java
@@ -0,0 +1,12 @@
+package ServerSide;
+
+import ClientSide.Client;
+
+/**
+ * @author Baha2r
+ * Date: 12/05/2019 03:07 AM
+ **/
+
+public interface OnDisconnected {
+    void onDisconnected(Client client);
+}
