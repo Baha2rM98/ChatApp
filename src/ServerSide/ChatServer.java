08edddf58f014382a93f043f3d60f043e7cb744c
Baha2r <baha2r.mirzazadeh98@gmail.com>
2019-12-06 01:44:45 +0330

    Defected... Still working

--- src/ServerSide/ChatServer.java
+++ src/ServerSide/ChatServer.java
@@ -1,5 +1,7 @@
 package ServerSide;
 
+import ClientSide.Client;
+
 import java.io.*;
 import java.net.*;
 import java.util.*;
@@ -10,23 +12,27 @@ import java.util.*;
  **/
 
 public class ChatServer implements OnConnected {
+    static Vector<ClientHandler> activeClients;
     private ServerSocket serverSocket;
     private Socket server;
-    static Vector<ClientHandler> activeClients;
     private int activeClientsNumber = 1;
+//    private String clientName;
 
     public ChatServer(int port) throws IOException {
         this.serverSocket = new ServerSocket(port);
-        System.out.println("Running the server...\nWaiting for client...\n");
+        System.out.println("Server is up!\nWaiting for client...\n");
         activeClients = new Vector<>();
         while (true) {
             onConnected();
             DataInputStream input = new DataInputStream(server.getInputStream());
             DataOutputStream output = new DataOutputStream(server.getOutputStream());
-            ClientHandler newClient = new ClientHandler(this.server, "client " + activeClientsNumber, input, output);
+//            this.clientName = Client.getNickName();
+            ClientHandler newClient = new ClientHandler(this.server, this.activeClientsNumber + "", input, output);
             activeClientsNumber++;
             activeClients.add(newClient);
             newClient.start();
+//            if (!newClient.isAlive())
+//                return;
         }
     }
 
@@ -42,7 +48,7 @@ public class ChatServer implements OnConnected {
     public void onConnected() {
         try {
             this.server = serverSocket.accept();
-            System.out.println("New client request received : " + server.toString());
+            System.out.println("New client joined at : " + server.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
