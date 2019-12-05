eacc0e66eedcb9980904df0a163160d4e89eb55d
Baha2r <baha2r.mirzazadeh98@gmail.com>
2019-12-06 01:42:18 +0330

    Send thread and read thread are in separated methods now

--- src/ClientSide/Client.java
+++ src/ClientSide/Client.java
@@ -10,55 +10,49 @@ import java.util.*;
  **/
 
 public class Client {
-    private String IP;
-    private int portNumber;
     private Scanner scn;
     private DataInputStream input;
     private DataOutputStream output;
+//    private static String nickName;
 
     public Client(String IP, int port) throws IOException {
-        this.scn = new Scanner(System.in);
-        this.IP = IP;
-        this.portNumber = port;
-        Socket client = new Socket(this.IP, portNumber);
+        Socket client = new Socket(IP, port);
         this.input = new DataInputStream(client.getInputStream());
         this.output = new DataOutputStream(client.getOutputStream());
-        Thread send = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    String message = scn.nextLine();
-                    try {
-                        output.writeUTF(message);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+        this.scn = new Scanner(System.in);
+//        System.out.println("Enter your nickname: ");
+//        nickName = scn.nextLine();
+        startSend().start();
+        startRead().start();
+    }
+
+    private Thread startSend() {
+        return new Thread(() -> {
+            while (true) {
+                String message = this.scn.nextLine();
+                try {
+                    this.output.writeUTF(message);
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
         });
+    }
 
-        Thread read = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    try {
-                        String message = input.readUTF();
-                        System.out.println(message);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+    private Thread startRead() {
+        return new Thread(() -> {
+            while (true) {
+                try {
+                    String message = this.input.readUTF();
+                    System.out.println(message);
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
         });
-        send.start();
-        read.start();
     }
 
-    public String getIP() {
-        return this.IP;
-    }
-
-    public int getPortNumber() {
-        return this.portNumber;
-    }
+//    public static String getNickName() {
+//        return nickName;
+//    }
 }
