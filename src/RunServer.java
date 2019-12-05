225e4d51fbc2f7dbb68707354cc0b22127980369
Baha2r <baha2r.mirzazadeh98@gmail.com>
2019-12-06 00:55:43 +0330

    Instance of Server.java was not necessary

--- src/RunServer.java
+++ src/RunServer.java
@@ -5,12 +5,12 @@ import java.io.IOException;
 /**
  * @author Baha2r
  * Date: 12/05/2019 09:18 PM
- *
+ * <p>
  * Runs server
  **/
 
 public class RunServer {
     public static void main(String[] args) throws IOException {
-        ChatServer server = new ChatServer(3000);
+        new ChatServer(3000);
     }
 }
